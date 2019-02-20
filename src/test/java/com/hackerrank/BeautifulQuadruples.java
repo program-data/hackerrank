605e73f5a7f2561a2e6c4ea57403cb466586b33b
michael <michael.malevannyy@gmail.com>
2019-02-21 00:52:37 +0300

    ++ но ее не доконца

--- src/test/java/com/hackerrank/BeautifulQuadruples.java
+++ src/test/java/com/hackerrank/BeautifulQuadruples.java
@@ -3,21 +3,51 @@ package com.hackerrank;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * https://www.hackerrank.com/challenges/xor-quadruples/problem
+ *
  * @author michael.malevannyy@gmail.com, 18.02.2019
  */
 
 public class BeautifulQuadruples {
     static int beautifulQuadruples(int a, int b, int c, int d) {
+        int q = 0;
+
+        // сорт вем
+        List<Integer> list = Arrays.asList(a,b,c,d);
+        Collections.sort(list);
+        a = list.get(0);
+        b = list.get(1);
+        c = list.get(2);
+        d = list.get(3);
 
 
-        // TODO
-        return 0;
+        for (int i = 1; i <= a; ++i) {
+            for (int j = i; j <= b; ++j) {
+                for (int k = j; k <= c; ++k) {
+                    for (int t = k; t <= d; ++t) {
+
+                        int x = i ^ j ^ k ^ t;
+                        if(x > 0)
+                            ++q;
+
+                        System.out.printf("%d %d %d %d = %d %s\n", i, j, k, t, x, x>0 ? String.format("(%d)", q) : "");
+
+                    }
+                }
+            }
+        }
+
+
+        return q;
     }
 
     @Test
     public void test() {
-        Assert.assertEquals(11, beautifulQuadruples(1, 2, 3, 4));
+        Assert.assertEquals(11, beautifulQuadruples(1, 2, 4, 3));
     }
 }
